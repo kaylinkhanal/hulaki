1f385db40563e019ad82d0f61191f8777b4dc1fa
kaylinkhanal10 <kaylin.k@foundationai.com>
2023-12-12 08:33:13 +0545

    open edit form

--- client/src/app/admin/categories/page.js
+++ client/src/app/admin/categories/page.js
@@ -23,7 +23,9 @@ const SignupSchema = Yup.object().shape({
 export const index = () => {
   const [categoryList, setCategoryList] = useState({})
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const showModal = () => {
+    const [selectedEditCat, setSelectedEditCat]= useState({})
+    const showModal = (item) => {
+      setSelectedEditCat(item)
       setIsModalOpen(true);
     };
     const handleOk = () => {
@@ -76,6 +78,60 @@ export const index = () => {
             categoryFetch();
           }
       };
+      const editCat = async(values) => {
+        const res = await fetch('http://localhost:4000/categories', {
+            method:'PUT', 
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(values)
+          })
+          const data = await res.json()
+            messageApi.open({
+              type: res.status == 200 ? 'success': 'error',
+              content: data.msg,
+            });
+            console.log(res)
+            if(res.status===200){
+              categoryFetch();
+            }
+        };
+
+  const EditForm= ()=>{
+    return (
+      <Formik
+      initialValues={selectedEditCat}
+      enableReinitialize
+      // validationSchema={SignupSchema}
+      onSubmit={values => {
+    
+        editCat(values)
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form >
+            <Field name="categoryName"  placeholder="categoryName" /> 
+              {errors.categoryName && touched.categoryName ? (
+                <div>{errors.categoryName}</div>
+              ) : null}
+              <br/>
+         
+              <Field name="maxWeight"  placeholder="maxWeight" /> 
+              {errors.maxWeight && touched.maxWeight ? (
+                <div>{errors.maxWeight}</div>
+              ) : null}
+              <br/>
+              <Field name="pricePerUnitKg"  placeholder="pricePerUnitKg" /> 
+              {errors.pricePerUnitKg && touched.pricePerUnitKg ? (
+                <div>{errors.pricePerUnitKg}</div>
+              ) : null}
+              <br/>
+              <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+    )
+  }
+
+  
   return(
   <div className='form'>
 
@@ -86,7 +142,7 @@ export const index = () => {
        
         categoryName : '',
         maxWeight: '',
-        PricePerUnitKm:''
+        PricePerUnitKg:''
         
       }}
       // validationSchema={SignupSchema}
@@ -104,8 +160,8 @@ export const index = () => {
           <Field name="maxWeight" type="text" placeholder="Enter your maxWeight" />
           {errors.maxWeight && touched.maxWeight ? <div>{errors.maxWeight}</div> : null}
           <br />
-          <Field name="PricePerUnitKm" type="text" placeholder="Enter your Price/Unit Km" />
-          {errors.PricePerUnitKm && touched.PricePerUnitKm ? <div>{errors.PricePerUnitKm}</div> : null}
+          <Field name="PricePerUnitKg" type="text" placeholder="Enter your Price/Unit kg" />
+          {errors.PricePerUnitKg && touched.PricePerUnitKg ? <div>{errors.PricePerUnitKg}</div> : null}
           <br />
           <button type="submit">Submit</button>
           <br />
@@ -119,14 +175,14 @@ export const index = () => {
       return  <Card.Grid style={gridStyle}>
       {id+1}.  {item.categoryName}
         <p onClick={()=>deleteCat(item._id)}>Del</p>
-        <p onClick={showModal}>Edit</p>
+        <p onClick={()=>showModal(item)}>Edit</p>
         </Card.Grid>
     })}
          </Card>
-         <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+         <Modal 
+           footer={null}
+         title="Basic Modal" open={isModalOpen}  onOk={handleOk} onCancel={handleCancel}>
+          <EditForm/>
       </Modal>
     </div>
 )};
