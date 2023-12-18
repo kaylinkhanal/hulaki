a4e9717d74fb8f594bdc2619fd44ae0d8f3f8671
kaylinkhanal10 <kaylin.k@foundationai.com>
2023-12-18 08:46:13 +0545

    added basic table

--- client/src/app/admin/user/page.js
+++ client/src/app/admin/user/page.js
@@ -1,7 +1,22 @@
-import React from "react";
+'use client'
+import React, {useState, useEffect} from "react";
+import Table from '../../../components/Table/page'
 const App=()=>{
+    const [userList, setUserList] = useState([])
+    const userFetch = async () => {
+        const res = await fetch(`http://localhost:4000/users`)
+        const data = await res.json()
+        setUserList(data.list)
+      }
+    
+    
+      useEffect(() => {
+        userFetch()
+      }, [])
     return(
-        <div>User-dashboard</div>
+        <div>
+            <Table userList={userList}/>
+        </div>
     )
 }
 export default App
--- client/src/components/Table/page.js
+++ client/src/components/Table/page.js
@@ -1,25 +1,48 @@
+'use client'
 import React from 'react';
 import { Space, Table, Tag } from 'antd';
-const columns = [
+const { Column, ColumnGroup } = Table;
+const data = [
   {
-    title: 'productName',
-    dataIndex: 'productName',
-    key: 'productName',
-    render: (text) => <a>{text}</a>,
+    key: '1',
+    firstName: 'John',
+    lastName: 'Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
   },
   {
-    title: 'price',
-    dataIndex: 'price',
-    key: 'price',
+    key: '2',
+    firstName: 'Jim',
+    lastName: 'Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    firstName: 'Joe',
+    lastName: 'Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
   },
 ];
+const App = (props) => (
+  <Table dataSource={props.userList}>
+    <Column title="Email" dataIndex="email" key="email" />
+  
+  <Column
+      title="Action"
+      key="action"
+      render={(_, record) => (
+        <Space size="middle">
+          <a>Edit </a>
+          <a>Delete</a>
+        </Space>
+      )}
+    /> 
 
-const App = (props) => {
-return (
-<div>
-        <Table columns={columns} dataSource={props.data} />
-</div>
-)
-
-};
+  </Table>
+);
 export default App;
