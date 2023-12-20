f58ec078c24b3ae1247abce81c4c35f23b4d1105
kaylinkhanal10 <kaylin.k@foundationai.com>
2023-12-20 09:37:09 +0545

    basic socket setup

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,11 +1,20 @@
-import React from 'react'
+'use client'
+import React, {useEffect} from 'react'
 import Home from './home/page'
 import NavBar from '../components/NavBar/page'
+import { io } from 'socket.io-client';
+const URL =  'http://localhost:4000';
+const socket = io(URL);
 function page() {
-  
-  //HOC: higher order components
+  useEffect(()=>{
+    socket.on('connection');
+   
+  },[])
   return (
     <div>
+      <button onClick={()=> socket.emit('msg','hello')}>
+        click
+        </button>
       <NavBar />
           <Home/>
     </div>
--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,30 @@ const express = require('express')
 const cors = require('cors')
 const connection = require('./db/connection')
 const app = express()
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
 
+const server = createServer(app);
+const io = new Server(server,{
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
 require('dotenv').config()
 const userRoute = require('./routes/user')
 const categoryRoute = require('./routes/categories')
 const orderRoute=require('./routes/order')
 
+io.on('connection', (socket) => {
+  console.log('conneted')
+  socket.on('msg', (msg) => {
+    console.log(msg);
+  });
+  ///
+});
+
 app.use(express.json())
+
 app.use(cors())
 app.use(userRoute)
 app.use(categoryRoute)
@@ -17,6 +34,6 @@ app.use(orderRoute)
 const port = process.env.PORT || 4000
 connection()
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
