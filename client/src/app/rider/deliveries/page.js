a26e773426c1b1a6ca06b09399be12a9481fd5e0
binod bastola <bndbstl@gmail.com>
2023-12-24 13:31:11 +0545

    Slight changes in rider/order page

---
+++ client/src/app/rider/deliveries/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+     deliveries
+    </div>
+  )
+}
+
+export default page
--- client/src/components/Table/page.js
+++ client/src/components/Table/page.js
@@ -1,31 +1,48 @@
 'use client'
 import React from 'react';
 import { Space, Table, Tag } from 'antd';
-const { Column} = Table;
+const { Column } = Table;
 
 const App = (props) => {
-  
-  return(
-  <Table dataSource={props.list}>
-    {props?.title?.map((item)=>{
-      return   <Column title={item} dataIndex={item} key={item} />
-    })}
-  
-  
-  {props.action && (
- <Column
- title="Action"
- key="action"
- render={(item) => (
-   <Space size="middle">
-     <a onClick={()=>props.onEdit(item)}>Edit </a>
-     <a onClick={()=>props.onDelete(item._id)}>Delete</a>
-   </Space>
- )}
-/> 
-  )}
- 
 
-  </Table>
-)};
+  return (
+    <Table dataSource={props.list}>
+      {props?.title?.map((item) => {
+        return <Column title={item} dataIndex={item} key={item} />
+      })}
+
+
+      {props.action && (
+        <Column
+          title="Action"
+          key="action"
+          render={(item) => (
+            <Space size="middle">
+              <a onClick={() => props.onEdit(item)}>Edit </a>
+              <a onClick={() => props.onDelete(item._id)}>Delete</a>
+            </Space>
+          )}
+        />
+      )}
+      {
+        props.rider &&
+        <Column
+          title="Status"
+          key="status"
+          render={(item) => (
+            <Space size="large">
+              <select style={{ width: '150px' }} onChange={(e)=>props.handleStatus(e,item)}>
+                <option value={item.status} disabled selected>{item.status}</option>
+                <option value="accepted">Accepted</option>
+                <option value="completed">Completed</option>
+              </select>
+            </Space>
+          )}
+        />
+      }
+
+
+    </Table>
+  )
+};
 export default App;
